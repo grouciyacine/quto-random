478de2bb40909b98ae7de81fdca3a97de82d0182
grouciyacine <wwwgrouciyacine@gmail.com>
2023-02-19 19:23:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data,setData]=useState()
+  // https//https://api.quotable.io/random
+  const getData=async()=>{
+const res=await fetch('https://api.quotable.io/random').then(res=>res.json()).then(res=>setData(res)).catch((err)=>console.log(err))
+  }
+  useEffect(()=>{
+getData()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className='container'>
+<header>
+  <title>quote</title>
+</header>
+
+
+  <div id="quote-box">
+  <h1 id='text'>
+ {data?.content}
+  </h1>
+<h5 id='author'>
+{data?.author}
+</h5>
+<div className='tt'> 
+<button id="new-quote" onClick={getData}>New Quote</button>
+<div className='ttt'>
+  <a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text'+data?.content} target='_blank' id='tweet-quote'>tweet</a>
+</div>
+
+</div>
+
+</div>
+
+
+
+</div>
   );
 }
 
